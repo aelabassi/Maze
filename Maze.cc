a0173f2aa775788cb83c399189f497d2d8b7d746
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-09-05 19:03:51 +0100

    render grounds

--- Maze.cc
+++ Maze.cc
@@ -1,4 +1,5 @@
 #include "game.hh"
+#include <vector>
 /**
  * main - Entry point
  * Return: 0 on success, 1 on failure
@@ -7,13 +8,25 @@ int main(int argc, char *argv[])
 {
     Game game;
     game.init("Maze", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
-    Entity platform0(100, 50, game.loadTexture("../assets/ground_grass_1.png"));
+
+    Entity entity0(0, 0, game.loadTexture("../assets/ground_grass_1.png"));
+    Entity entity1(30, 0, game.loadTexture("../assets/ground_grass_1.png"));
+    Entity entity2(30, 30, game.loadTexture("../assets/ground_grass_1.png"));
+    Entity entity3(30, 60, game.loadTexture("../assets/ground_grass_1.png"));
+    std::vector<Entity> entities;
+    entities.push_back(entity0);
+    entities.push_back(entity1);
+    entities.push_back(entity2);
+    entities.push_back(entity3);
     while (game.running())
     {
         game.handleEvents();
         game.update();
         game.clear();
-        game.render_texture(platform0);
+        for (auto &entity : entities)
+        {
+            game.render_texture(entity);
+        }
         game.display();
     }
     game.clean();
