8ecfd9d01efeab4518af6bf064f0995b7efbdc48
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-09-25 11:13:03 +0100

    more textures

--- src/textures.c
+++ src/textures.c
@@ -7,7 +7,7 @@ static const char *textureFileNames[NUM_TEXTURES] = {
         TEXTURE_PATH("graystone.png"),
         TEXTURE_PATH("colorstone.png"),
         TEXTURE_PATH("redbrick.png"),
-        TEXTURE_PATH("yajor.png"),
+        TEXTURE_PATH("wood.png"),
         TEXTURE_PATH("eagle.png"),
 };
 
